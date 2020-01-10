e9553ddee5f43660fae50106d103d5ea82c93f40
Joyel Shaju <JShaju@spartaglobal.com>
2020-01-10 17:19:43 +0000

    first commit

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -4,7 +4,9 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
+
 require 'spec_helper'
+
 
 describe 'mongo::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
@@ -13,17 +15,16 @@ describe 'mongo::default' do
     platform 'ubuntu', '18.04'
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      expect{chef_run}.to_not raise_error
     end
-  end
-
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'updates all sources' do
+      expect(chef_run).to update_apt_update("update")
+    end
+    it 'should add mongo to the source list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
+    end
+    it 'should install mongo' do
+      expect(chef_run).to upgrade_package('mongodb-org')
     end
   end
 end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -11,6 +11,18 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package 'mongodb-org' do
+  it { should be_installed }
+  its('version') { should match /3\./}
+end
+
+
+describe service "mongodb" do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+describe port(27017) do
+  it { should be_listening }
+  its('processes') { should include '0.0.0.0'}
 end
