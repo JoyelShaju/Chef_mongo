ec069667fee546be738c02ad309bc9fee2d5b9be
Joyel Shaju <JShaju@spartaglobal.com>
2020-01-22 11:02:29 +0000

    First commit

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,4 +1,4 @@
-# InSpec test for recipe mongo::default
+# InSpec test for recipe mongo_cookbook::default
 
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
@@ -11,18 +11,21 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe package 'mongodb-org' do
-  it { should be_installed }
-  its('version') { should match /3\./}
+describe port(80), :skip do
+  it { should_not be_listening }
 end
 
-
-describe service "mongodb" do
+describe service "mongod" do
   it { should be_running }
   it { should be_enabled }
 end
 
+describe package "mongodb-org" do
+  its('version') { should match /3\./ }
+end
+
 describe port(27017) do
   it { should be_listening }
-  its('processes') { should include '0.0.0.0'}
+  its('processes') { should cmp 'mongod'}
+  its('addresses') { should include '0.0.0.0' }
 end
